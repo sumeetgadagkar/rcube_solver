4c09b38a91662bf6868ae3d0f35903e3dde51428
zoroppz <zoro.ppz.g@gmail.com>
2020-04-12 17:48:32 +0530

    Initial commit 2

---
+++ cmake-build-debug/src/CMakeFiles/cube.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sumeet/CLionProjects/rcube_solver/src/Cube.cpp" "/home/sumeet/CLionProjects/rcube_solver/cmake-build-debug/src/CMakeFiles/cube.dir/Cube.cpp.o"
+  "/home/sumeet/CLionProjects/rcube_solver/src/Face.cpp" "/home/sumeet/CLionProjects/rcube_solver/cmake-build-debug/src/CMakeFiles/cube.dir/Face.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/src/CMakeFiles/cube.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cube.dir/Cube.cpp.o"
+  "CMakeFiles/cube.dir/Face.cpp.o"
+  "libcube.a"
+  "libcube.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cube.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
